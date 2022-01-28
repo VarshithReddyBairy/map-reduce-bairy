d017bb83ee98e901e55f1dcc1f281002dd714820
VarshithReddyBairy <77808640+VarshithReddyBairy@users.noreply.github.com>
2022-01-28 14:35:05 -0600

    added sorting based on values

--- 02Sorter.py
+++ 02Sorter.py
@@ -6,17 +6,15 @@ def my_sort(line):
     return amount
 
 n = open("01Mapper.txt","r")  # open file, read-only
-s = open("01Sorter.txt", "w") # open file, write
-
+o = open("02Sorter.txt", "w") # open file, write
 
 lines = n.readlines()
 
-lines.sort(key = my_sort , reverse= True)
+# lines.sort(key = my_sort , reverse= True)
 # lines.sort(key = lambda x : x[1])
-# lines.sort()
+lines.sort()
 
 for line in lines:
-	s.write(line)
+	o.write(line)
 n.close()
-s.close()
-
+o.close()
